cf153e07b7e0ba0957eb5ffb326599ae4a1a11b1
Daniel de Souza Neri <danielneris01@gmail.com>
2019-06-23 13:23:07 -0300

    Buscando produtos da API

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Main from './pages/main/index';
 import Header from './components/Header/index';
 import './styles.css';
 
 const App = () => (
   <div className="App">
     <Header></Header>
+    <Main></Main>
   </div>
 );
 
